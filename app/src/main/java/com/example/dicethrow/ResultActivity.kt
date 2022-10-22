bd96a300d5c2e529f7e62aa699360c0021acc170
Piotr <hynas321@gmail.com>
2022-10-22 16:30:56 +0200

    Add animation variables, reveal result and "Go back" button after having finished animating dice

--- app/src/main/java/com/example/dicethrow/ResultActivity.kt
+++ app/src/main/java/com/example/dicethrow/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.dicethrow
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -23,8 +24,10 @@ class ResultActivity : AppCompatActivity() {
 
     private val maximumDotsNumber: Int = 6
     private val oneDiceAnimationYOffset: Float = -350f
-    private val twoDiceAnimationYOffset: Float = -600f
+    private val twoDiceAnimationYOffset: Float = -700f
     private val threeDiceAnimationYOffset: Float = -400f
+    private val diceAnimationDurationMs: Long = 1300
+    private val diceAnimationRotation: Float = 180f
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -47,12 +50,11 @@ class ResultActivity : AppCompatActivity() {
         val dieOneDotsCount = Random.nextInt(1, maximumDotsNumber)
         val dieTwoDotsCount = Random.nextInt(1, maximumDotsNumber)
         val dieThreeDotsCount = Random.nextInt(1, maximumDotsNumber)
-        var diceDotsSum = 0
+        val diceDotsSum: Int
 
         when (diceCount) {
             DiceCount.ONE -> {
                 diceDotsSum = dieOneDotsCount
-                textViewDiceDotsSumValue.text = diceDotsSum.toString()
 
                 resultActivityModifier.setDieImageViewByDotsCount(
                     imageViewDieOne,
@@ -61,27 +63,26 @@ class ResultActivity : AppCompatActivity() {
 
                 imageViewDieOne.visibility = View.VISIBLE
 
-                imageViewDieOne.animate().setDuration(1500)
-                    .translationYBy(oneDiceAnimationYOffset).rotation(180f)
+                imageViewDieOne.animate().setDuration(diceAnimationDurationMs)
+                    .translationYBy(oneDiceAnimationYOffset).rotation(diceAnimationRotation)
             }
             DiceCount.TWO -> {
                 diceDotsSum = dieTwoDotsCount + dieThreeDotsCount
-                textViewDiceDotsSumValue.text = diceDotsSum.toString()
 
-                resultActivityModifier.setDieImageViewByDotsCount(imageViewDieTwo, dieOneDotsCount)
-                resultActivityModifier.setDieImageViewByDotsCount(imageViewDieThree, dieTwoDotsCount)
+                resultActivityModifier.setDieImageViewByDotsCount(imageViewDieTwo, dieTwoDotsCount)
+                resultActivityModifier.setDieImageViewByDotsCount(imageViewDieThree, dieThreeDotsCount)
 
                 imageViewDieTwo.visibility = View.VISIBLE
                 imageViewDieThree.visibility = View.VISIBLE
 
-                imageViewDieTwo.animate().setDuration(1500)
-                    .translationYBy(twoDiceAnimationYOffset).rotation(180f)
-                imageViewDieThree.animate().setDuration(1500)
-                    .translationYBy(twoDiceAnimationYOffset).rotation(180f)
+                imageViewDieTwo.animate().setDuration(diceAnimationDurationMs)
+                    .translationYBy(twoDiceAnimationYOffset).rotation(diceAnimationRotation)
+
+                imageViewDieThree.animate().setDuration(diceAnimationDurationMs)
+                    .translationYBy(twoDiceAnimationYOffset).rotation(diceAnimationRotation)
             }
             DiceCount.THREE -> {
                 diceDotsSum = dieOneDotsCount + dieTwoDotsCount + dieThreeDotsCount
-                textViewDiceDotsSumValue.text = diceDotsSum.toString()
 
                 resultActivityModifier.setDieImageViewByDotsCount(imageViewDieOne, dieOneDotsCount)
                 resultActivityModifier.setDieImageViewByDotsCount(imageViewDieTwo, dieTwoDotsCount)
@@ -91,23 +92,27 @@ class ResultActivity : AppCompatActivity() {
                 imageViewDieTwo.visibility = View.VISIBLE
                 imageViewDieThree.visibility = View.VISIBLE
 
-                imageViewDieOne.animate().setDuration(1500)
-                    .translationYBy(threeDiceAnimationYOffset).rotation(180f)
-                imageViewDieTwo.animate().setDuration(1500)
-                    .translationYBy(threeDiceAnimationYOffset).rotation(180f)
-                imageViewDieThree.animate().setDuration(1500)
-                    .translationYBy(threeDiceAnimationYOffset).rotation(180f)
+                imageViewDieOne.animate().setDuration(diceAnimationDurationMs)
+                    .translationYBy(threeDiceAnimationYOffset).rotation(diceAnimationRotation)
+
+                imageViewDieTwo.animate().setDuration(diceAnimationDurationMs)
+                    .translationYBy(threeDiceAnimationYOffset).rotation(diceAnimationRotation)
+
+                imageViewDieThree.animate().setDuration(diceAnimationDurationMs)
+                    .translationYBy(threeDiceAnimationYOffset).rotation(diceAnimationRotation)
             }
         }
-
-        textViewDotsNumber.visibility = View.VISIBLE
-        textViewDiceDotsSumValue.visibility = View.VISIBLE
-        buttonGoBack.visibility = View.VISIBLE
 
         buttonGoBack.setOnClickListener {
             val intentMain = Intent(this, MainActivity::class.java)
             startActivity(intentMain)
         }
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            buttonGoBack.visibility = View.VISIBLE
+            textViewDiceDotsSumValue.text = diceDotsSum.toString()
+        }, diceAnimationDurationMs)
     }
 
     override fun onBackPressed() {
